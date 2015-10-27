78071ee7813f6aad9e407f119119bcd753b6ef73
Nayan Kumar Konaje <nayankk@motorola.com>
2015-10-26 22:37:50 -0700

    Display travel time. Note that computation of travel time is performed on device now, needs to be changed to compute this on app engine.

--- app/src/main/java/com/xc0ffee/taxicab/app/MapsFragment.java
+++ app/src/main/java/com/xc0ffee/taxicab/app/MapsFragment.java
@@ -13,11 +13,13 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.xc0ffee.taxicab.R;
@@ -84,14 +86,16 @@ public class MapsFragment extends Fragment implements DriverPositionManager.Driv
     @TargetApi(23)
     private void setupMapIfNeeded() {
         if (mMap == null) {
-            mMap = ((MapFragment) getChildFragmentManager().findFragmentById(R.id.location_map)).getMap();
+            mMap = ((MapFragment) getChildFragmentManager().
+                    findFragmentById(R.id.location_map)).getMap();
             if (mMap != null) {
                 if (TaxiCabUtils.isM()) {
                     if (getActivity().checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)
                             == PackageManager.PERMISSION_GRANTED) {
                         setUpMap();
                     } else {
-                        getActivity().requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        getActivity().requestPermissions(
+                                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
                                 TaxiCabMainActivity.LOCATION_PERMISSION_REQUEST_ID);
                     }
                 } else
@@ -114,6 +118,26 @@ public class MapsFragment extends Fragment implements DriverPositionManager.Driv
                 LatLng latLng = new LatLng(latitude, longitude);
                 CameraUpdate yourLocation = CameraUpdateFactory.newLatLngZoom(latLng, 14);
                 mMap.moveCamera(yourLocation);
+                mMap.setOnCameraChangeListener(new GoogleMap.OnCameraChangeListener() {
+                    @Override
+                    public void onCameraChange(CameraPosition cameraPosition) {
+                        Log.d("NAYAN", "OnCameraChange ");
+                        new ComputeTravelTime(getActivity(), cameraPosition.target,
+                                new ComputeTravelTime.SelectedDriverDetails() {
+                                    @Override
+                                    public void onDriverSelected(String driveId,
+                                                                 final int travelTime) {
+                                        getActivity().runOnUiThread(new Runnable() {
+                                            @Override
+                                            public void run() {
+                                                ((TextView)getView().findViewById(R.id.travel_time))
+                                                        .setText(Integer.toString(travelTime/60));
+                                            }
+                                        });
+                                    }
+                                });
+                    }
+                });
             }
         } catch (SecurityException e) {
             Log.d(TAG, "Unable to access localation permission");
@@ -132,7 +156,8 @@ public class MapsFragment extends Fragment implements DriverPositionManager.Driv
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                           int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == TaxiCabMainActivity.LOCATION_PERMISSION_REQUEST_ID) {
             setUpMap();
